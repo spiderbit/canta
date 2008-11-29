eeca6d0215ca6d66e4e0d439d034ef6a8867034f
bruecher <bruecher@benza.localdomain>
2008-11-29 17:24:36 +0100

    Refactored update-media.py

--- scripts/update-media.py
+++ scripts/update-media.py
@@ -1,137 +1,134 @@
+#!/usr/bin/env python
+"""
+$Id$
+Downloads themes and music from the homepage
+"""
 
-import os
+import base64
 import md5
+import os
 import tarfile
-
-class MyWeb:
-    def __init__(self, host, webuser=None, webpass=None):
-        self.host = host
-        self.webuser = webuser
-        self.webpass = webpass
-
-    def download_file(self, web_file, lokal_file=None):
-        """
-        Downloads a file
-        """
-
-        import urllib2
-        import base64
-
-        if lokal_file == None:
-            lokal_file = web_file.split('/')[-1]
-
-        request =  urllib2.Request(self.host + '/' + web_file)
-
-        if self.webuser:
-            base64string = base64.encodestring('%s:%s' % (self.webuser, self.webpass))[:-1]
-            request.add_header("Authorization", "Basic %s" % base64string)
-
-        htmlFile = urllib2.urlopen(request)
-        htmlData = htmlFile.read()
-        htmlFile.close()
+import urllib2
 
 
-        f = file(lokal_file, "wb")
-        f.write(htmlData)
-        f.close() 
+class File(object):
+	def __init__(self, filename):
+		self.__filename = filename
+
+	def readlines(self):
+		f = file(self.__filename, 'r')
+		d = f.readlines()
+		f.close()
+		return d
+
+	def write(self, data):
+		f = file(self.__filename, 'wb')
+		f.write(data)
+		f.close()
+
+	def md5sum(self):
+		f = file(self.__filename, 'rb')
+		md5sum = md5.new()
+		while True:
+			d = f.read(1024)
+			if len(d) is 0:
+				break
+			md5sum.update(d)
+
+		f.close()
+		return md5sum.hexdigest()
 
 
+class Directory(object):
+	def __init__(self, directory):
+		self.__directory = directory
+
+	def make(self):
+		if not os.path.exists(self.__directory):
+			os.mkdir(self.__directory)
+		elif os.path.isfile(self.__directory):
+			raise OSError('ERROR: a file with the same ' \
+				'name as the desired dir, "%s", ' \
+				'already exists.' % self.__directory)
+
+	def change(self):
+		os.chdir(self.__directory)
 
 
+class Internet(object):
+	def __init__(self, username = None, password = None):
+		self.__username, self.__password = username, password
+
+	def _getData(self, request):
+		u = urllib2.urlopen(request)
+		d = u.read()
+		u.close()
+		return d
+
+	def download(self, remote, local = None):
+		if local is None: local = remote.split('/')[-1]
+
+		print 'Dowloading %s' % remote
+		request = urllib2.Request(remote)
+
+		if self.__username:
+			request.add_header('Authorization', 'Basic %s' %
+				base64.encodestring('%s:%s' % (
+				self.__username, self.__password))[:-1])
+
+		File(local).write(self._getData(request))
 
 
-def _mkdir(newdir):
-	if os.path.isdir(newdir):
-		pass
-	elif os.path.isfile(newdir):
-		raise OSError("ERROR: a file with the same name as the desired " \
-							"dir, '%s', already exists." % newdir)
-	else:
-		os.mkdir(newdir)
+def download(url, directory, username = None, password = None):
+	MD5SUMS, TMP  = 'MD5SUMS', 'tmp'
 
-def get_hex(filex):
-	m = md5.new()
-	f = file(filex, 'rb') # open in binary mode
-	while True:
-		t = f.read(1024)
-		if len(t) == 0: break # end of file
-		m.update(t)
-	return m.hexdigest()
+	Directory(TMP).make()
+	Directory(TMP + os.path.sep + directory).make()
+	Directory(TMP + os.path.sep + directory).change()
 
-def do_it(base_url, dir, target_dir, username, password):
+	if os.path.isfile(MD5SUMS): os.unlink(MD5SUMS)
 
-	newdir = 'tmp'
+	internet = Internet(username, password)
+	try:
+		internet.download(url + MD5SUMS)
+	except urllib2.URLError:
+		print 'File not found or server down?'
+		return False
+	except urllib2.HTTPError:
+		print 'Wrong password?'
+		return False
 
+	for line in File(MD5SUMS).readlines():
+		(md5sum, filename) = line.split()
+		print('Checking (%s) %s' % (md5sum, filename))
 
-	seperator='/'
-	MD5SUMS='MD5SUMS'
+		if not os.path.isfile(filename):
+			print '%s not found.' % filename
+			internet.download(url + filename)
 
-	#url_path = base_url + seperator + dir + seperator
-	url_path = dir + seperator
-	_mkdir(newdir)
-	os.chdir(newdir)
-	_mkdir(dir)
-	os.chdir(dir)
-	if os.path.isfile(MD5SUMS):
-		os.unlink(MD5SUMS)
-	#os.system(dl_app +' '+ url_path + MD5SUMS)
-	web = MyWeb(base_url, username, password)
-	web.download_file(url_path + MD5SUMS, MD5SUMS)
-	#os.system('cat' +' '+ MD5SUMS)
-	md5file = file(MD5SUMS)
-	lines = md5file.readlines()
-	for line in lines:
-		words = line.split()
-		
-		if not os.path.isfile(words[1]):	# path must be checked
-			print "Downloading " + words[1] + ", because you don't have it ..."
-			web.download_file(url_path + words[1])
-			print "Done."
-		elif words[0] != get_hex(words[1]):
-			print "Downloading " + words[1] + ", update or corruption ..."
-			os.unlink(words[1])
-			web.download_file(url_path + words[1])
-			print "Done."
-		if words[0] == get_hex(words[1]):
-			print "File " + words[1] + "\t\tis ok."
-			#os.chdir('songs/')
-			#print 'tar xZf ' + words[1] + ' --directory=../../' + target_dir
-			tar=tarfile.open(words[1], mode="r:gz")
-			#tar.makedir(tar.getmembers(),"testtttt")
-			members = tar.getmembers()
-			for member in members:
-				tar.extract(member, "../../"+ target_dir)
-				#  	map(tar.extract, tar.getmembers())
-			#os.system('tar xzf ' + words[1] + ' --directory=../../' + target_dir)
-			#os.chdir('../tmp/')
+		print '\tProcessing %s' % filename
+		if File(filename).md5sum() == md5sum:
+			tar = tarfile.open(filename, mode = 'r:gz')
+			for m in tar.getmembers():
+				tar.extract(m,
+					('..' + os.path.sep) * 2 + directory)
+			print '\t\tUnpacked.'
 		else:
-			print "ERROR: file  " + words[1] + " could not be synced!!!"
-	os.chdir('..')
-	os.chdir('..')
-	#os.system('tar xzf ' + words[1])
+			print '\t\tChecksum doesn\'t match. Giving up...'
+
+	Directory('..' + os.path.sep + '..').change()
 
 
+if __name__ == '__main__':
+	username, password = None, None
 
-base_url="http://www.canta-game.org/downloads/songs"
-sub_urls=['unfree', 'free', 'tests']
+	username = raw_input('Username: ')
+	password = raw_input('Password: ')
 
+	url = 'http://www.canta-game.org/downloads'
+	download(url + '/themes/', 'themes')
 
-	
-
-username = raw_input('Username: ')
-password = raw_input('Password: ')
-
-
-md5_urls=[]
-
-for dir in sub_urls:
-	do_it(base_url, dir, target_dir='songs', username=username, password=password)
-
-
-base_url="http://www.canta-game.org/downloads/"
-
-do_it(base_url, dir='themes', target_dir='themes', username=username, password=password)
-	
-	
-
+	for directory in ['unfree', 'free', 'tests']:
+		download(url + '/songs/' + directory + '/', 'songs',
+			username, password)
 
