f79de2ff24606c1685e86113526bd0109f4d948d
Stefan Huchler <stefan@canta-game.org>
2009-01-30 20:42:24 +0100

    add header to SongLoader

--- canta/song/song_loader.py
+++ canta/song/song_loader.py
@@ -1,3 +1,21 @@
+#! /usr/bin/python -O
+# -*- coding: utf-8 -*-
+#
+#    CANTA - A free entertaining educational software for singing
+#    Copyright (C) 2007  S. Huchler, A. Kattner, F. Lopez
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 from canta.song.song import Song
@@ -35,31 +53,31 @@ class SongLoader:
             dirs.sort()
             for dir in dirs:
                 absolute_path = os.path.join(songs_path,dir)
-                    
+
                 for root, dirs, files in os.walk(absolute_path):
-                    
+
                     for file in files:
                         file = file.decode('utf-8')
                         ''' removed gif and bmp here (soya only supports jpeg and png)'''
                         valid_picture_formats = ['jpg', 'jpeg', 'png']
                         valid_sound_formats = ['ogg', 'mp3']
-                            
+
                         lower_file = file.lower()
                         if lower_file == "desc.txt":
                             pass
                         elif lower_file.endswith('.txt'):
 
 
-                            
+
 
                             # The song:
                             song = Song(debug=self.debug, path = root, file=file)
                             song.read_from_us( type="headers")
-                            
+
                             #print "Scanning Directory:\t<" + song.path + ">\n"
                             files = self.unicode(files)
                             self.search_songs(valid_picture_formats, song, files, 'cover')
-                            
+
 
                             if 'cover' in song.info:
                                 #print "\t\tFOUND Cover <", song.info['cover'], ">"
@@ -71,9 +89,9 @@ class SongLoader:
                             if 'mp3' in song.info and song.info != None:
                                 mp3s+=1
                                 songs.append(song)
-                            
-                                
-                            
+
+
+
         #print mp3s, " Songs with ", covers, " valid Covers found!"
         return songs
 
@@ -87,7 +105,7 @@ class SongLoader:
         files_with_right_format = []
         search = False
         '''file in txt file is right'''
-        if check_item in song.info and song.info[check_item] != None and song.info[check_item] in files: 
+        if check_item in song.info and song.info[check_item] != None and song.info[check_item] in files:
             return #found = True
         elif check_item in song.info:	# k there there is at least some info for this item
             if song.info[check_item] != None: # k there is something i can search
@@ -95,9 +113,9 @@ class SongLoader:
         for format in valid_formats:
             ''' if something about that stands in the file
             when not searching only files with right extention '''
-            if search:	
+            if search:
                 item = self.remove_extention(song.info[check_item]) + "." + format
-            
+
             for file in files:
                 #print " ", item, file
                 if search and item.lower() == file.lower():
@@ -105,31 +123,31 @@ class SongLoader:
                     found = True
                     song.info[check_item] = file
                     break
-                # search for the right endings if thats the wrong file i cant detect it, 
+                # search for the right endings if thats the wrong file i cant detect it,
                 #but better sometimes  a wrong file instead of no file found
                 elif file.lower().endswith(format):
                     #print "found a file with right ending", file
                     files_with_right_format.append(file)
                 else:
                     pass
-                
+
             if found:
                 break
         else:
-            ''' i take it if its more i think i better dont take it 
+            ''' i take it if its more i think i better dont take it
             because its to dangerous that its not the right file.'''
-            if len(files_with_right_format) == 1: 
-                '''print "FOUND 1 file with right ending but wrong name 
+            if len(files_with_right_format) == 1:
+                '''print "FOUND 1 file with right ending but wrong name
                 i take what i get:<"+files_with_right_format[0]+">"'''
                 song.info[check_item] = files_with_right_format[0]
-                
+
             else:
-                '''print "found some files with right ending for 
+                '''print "found some files with right ending for
                 "+ check_item + ": ", len (files_with_right_format)'''
                 for file in files_with_right_format:
                     ''' bad hack but the covers have often a substring [CO] '''
                     if (file.lower().find(check_item)) != -1 \
-                            or (file.lower()).find("co") != -1: 
+                            or (file.lower()).find("co") != -1:
                         #print "find cover or co in file", file.lower().find(check_item)
                         song.info[check_item] = file
                         break
@@ -148,4 +166,3 @@ class SongLoader:
     def remove_extention(self, name):
         non_ext_file = (name.split('.'))[:-1]
         return '.'.join(non_ext_file)
-
